04069a7d2bfd6df0c18e38a2af9f4a0264607bc7
oloko64 <naldoepingo1@gmail.com>
2021-10-31 19:47:24 -0300

    feat: add data file for navbar components

---
+++ src/data/menu/navbar.js
@@ -0,0 +1,12 @@
+export const TOP_MENU_DATA = {
+  HOME: 'HOME',
+  ABOUT: 'SOBRE',
+  PORTIFOLIO: 'Portifólio',
+};
+
+export const DRAWER_DATA = {
+  FOO: 'foo',
+  BAR: 'Bar',
+  FIZZ: 'Fizz',
+  BUZZ: 'Buzz',
+};
