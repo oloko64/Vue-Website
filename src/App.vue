3773b197410a302d3c587146aa75e4d3a999a664
oloko64 <naldoepingo1@gmail.com>
2021-10-31 21:07:55 -0300

    feat: add logic to change route

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <v-app>
-    <v-app-bar app elevation="0">
+    <v-app-bar app flat height="64">
       <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-      <v-btn text height="64">
+      <v-btn text height="64" @click="handleHome">
         <h2>{{ TOP_MENU_DATA.HOME }}</h2>
       </v-btn>
       <v-spacer></v-spacer>
-      <v-btn text height="64">{{ TOP_MENU_DATA.ABOUT }}</v-btn>
-      <v-btn text height="64">{{ TOP_MENU_DATA.PORTIFOLIO }}</v-btn>
+      <v-btn text height="64" @click="handleAbout">{{ TOP_MENU_DATA.ABOUT }}</v-btn>
+      <v-btn text height="64">{{ TOP_MENU_DATA.PORTFOLIO }}</v-btn>
     </v-app-bar>
-    <v-navigation-drawer v-model="drawer" absolute temporary>
+    <v-navigation-drawer v-model="drawer" absolute temporary app>
       <drawer-menu-items />
     </v-navigation-drawer>
     <v-main>
@@ -40,6 +40,22 @@ export default {
   watch: {
     group() {
       this.drawer = false;
+    },
+  },
+  methods: {
+    // TODO - Improve logic
+    handleAbout() {
+      const path = '/about';
+      if (this.$route.path !== path) {
+        this.$router.push(path);
+      }
+      // this.$router.push({ name: 'About' });
+    },
+    handleHome() {
+      const path = '/';
+      if (this.$route.path !== path) {
+        this.$router.push(path);
+      }
     },
   },
 };
