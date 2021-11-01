edcdef6704c7cad7c1dce20d2a75bab2d08fd2f2
oloko64 <naldoepingo1@gmail.com>
2021-11-01 14:43:23 -0300

    feat: create store for window size

--- src/store/index.js
+++ src/store/index.js
@@ -5,11 +5,20 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    windowSize: { x: 0, y: 0 },
   },
   mutations: {
+    updateWindowSize(state, data) {
+      state.windowSize = data;
+    },
+  },
+  getters: {
+    windowsSizeStore: ({ windowSize }) => windowSize,
   },
   actions: {
+    updateWindowSize: (context, payload) => {
+      context.commit('updateWindowSize', payload);
+    },
   },
-  modules: {
-  },
+  modules: {},
 });
