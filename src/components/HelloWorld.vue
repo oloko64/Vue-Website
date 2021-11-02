66a645205b2c8c1ef9764953111962989bcb3162
oloko64 <naldoepingo1@gmail.com>
2021-11-02 20:57:23 -0300

    feat: add background image parallax

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,101 +1,105 @@
 <template>
   <div class="hello-word">
-    <v-container>
-      <v-row class="text-center">
-        <v-col cols="12">
-          <v-img
-            :src="require('../assets/logo.svg')"
-            class="my-3"
-            contain
-            height="200"
-          />
-        </v-col>
+    <!-- TODO - The height needs to be specified page by page taking into account the image size -->
+    <v-parallax :src="backgroundImage" height="900">
+      <v-container>
+        <v-row class="text-center">
+          <v-col cols="12">
+            <v-img
+              :src="require('../assets/logo.svg')"
+              class="my-3"
+              contain
+              height="200"
+            />
+          </v-col>
 
-        <v-col class="mb-4">
-          <text-field-custom></text-field-custom>
-          <h1 class="display-2 font-weight-bold mb-3">
-            Welcome to Vuetify
-          </h1>
+          <v-col class="mb-4">
+            <text-field-custom></text-field-custom>
+            <h1 class="display-2 font-weight-bold mb-3">
+              Welcome to Vuetify
+            </h1>
 
-          <p class="subheading font-weight-regular">
-            For help and collaboration with other Vuetify developers,
-            <br>please join our online
-            <a
-              href="https://community.vuetifyjs.com"
-              target="_blank"
-            >Discord Community</a>
-          </p>
-        </v-col>
+            <p class="subheading font-weight-regular">
+              For help and collaboration with other Vuetify developers,
+              <br>please join our online
+              <a
+                href="https://community.vuetifyjs.com"
+                target="_blank"
+              >Discord Community</a>
+            </p>
+          </v-col>
 
-        <v-col
-          class="mb-5"
-          cols="12"
-        >
-          <h2 class="headline font-weight-bold mb-3">
-            What's next?
-          </h2>
+          <v-col
+            class="mb-5"
+            cols="12"
+          >
+            <h2 class="headline font-weight-bold mb-3">
+              What's next?
+            </h2>
 
-          <v-row justify="center">
-            <a
-              v-for="(next, i) in whatsNext"
-              :key="i"
-              :href="next.href"
-              class="subheading mx-3"
-              target="_blank"
-            >
-              {{ next.text }}
-            </a>
-          </v-row>
-        </v-col>
+            <v-row justify="center">
+              <a
+                v-for="(next, i) in whatsNext"
+                :key="i"
+                :href="next.href"
+                class="subheading mx-3"
+                target="_blank"
+              >
+                {{ next.text }}
+              </a>
+            </v-row>
+          </v-col>
 
-        <v-col
-          class="mb-5"
-          cols="12"
-        >
-          <h2 class="headline font-weight-bold mb-3">
-            Important Links
-          </h2>
+          <v-col
+            class="mb-5"
+            cols="12"
+          >
+            <h2 class="headline font-weight-bold mb-3">
+              Important Links
+            </h2>
 
-          <v-row justify="center">
-            <a
-              v-for="(link, i) in importantLinks"
-              :key="i"
-              :href="link.href"
-              class="subheading mx-3"
-              target="_blank"
-            >
-              {{ link.text }}
-            </a>
-          </v-row>
-        </v-col>
+            <v-row justify="center">
+              <a
+                v-for="(link, i) in importantLinks"
+                :key="i"
+                :href="link.href"
+                class="subheading mx-3"
+                target="_blank"
+              >
+                {{ link.text }}
+              </a>
+            </v-row>
+          </v-col>
 
-        <v-col
-          class="mb-5"
-          cols="12"
-        >
-          <h2 class="headline font-weight-bold mb-3">
-            Ecosystem
-          </h2>
+          <v-col
+            class="mb-5"
+            cols="12"
+          >
+            <h2 class="headline font-weight-bold mb-3">
+              Ecosystem
+            </h2>
 
-          <v-row justify="center">
-            <a
-              v-for="(eco, i) in ecosystem"
-              :key="i"
-              :href="eco.href"
-              class="subheading mx-3"
-              target="_blank"
-            >
-              {{ eco.text }}
-            </a>
-          </v-row>
-        </v-col>
-      </v-row>
-    </v-container>
+            <v-row justify="center">
+              <a
+                v-for="(eco, i) in ecosystem"
+                :key="i"
+                :href="eco.href"
+                class="subheading mx-3"
+                target="_blank"
+              >
+                {{ eco.text }}
+              </a>
+            </v-row>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-parallax>
   </div>
 </template>
 
 <script>
 import TextFieldCustom from '@/components/input/TextFieldCustom.vue';
+import DarkImg from '@/assets/img/purple.png';
 
 export default {
   name: 'HelloWorld',
@@ -154,6 +158,11 @@ export default {
       },
     ],
   }),
+  computed: {
+    backgroundImage() {
+      return DarkImg;
+    },
+  },
 };
 </script>
 
