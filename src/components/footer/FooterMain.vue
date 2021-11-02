0170725eb2a7077f5e649ff2a256b110b7493e49
vertocode <evertonvanoni1@gmail.com>
2021-11-01 22:05:39 -0300

    feat: att footer and colors

--- src/components/footer/FooterMain.vue
+++ src/components/footer/FooterMain.vue
@@ -1,30 +1,30 @@
 <template>
-  <v-footer class="mainFooter" padless>
-    <v-container>
-      <v-row no-gutters>
-        <v-col class="text-center mt-4" cols="7">
-          <h2>{{ SOBRE_USER_DATA.SOBRE }}</h2>
-          <p>{{ SOBRE_USER_DATA.DESCRIPTION }}</p>
-        </v-col>
-        <v-col cols="auto" class="text-lg-center">
-          <h3>{{ LINKS_USER_DATA.LINKS }}</h3>
-          <a :href="LINKS_USER_DATA.LINK_GITHUB" target="_blank">{{ LINKS_USER_DATA.GITHUB }}</a>
-          <br/>
-          <a :href="LINKS_USER_DATA.LINK_LINKEDIN" target="_blank">
-            {{ LINKS_USER_DATA.LINKEDIN }}
-          </a>
-          <br/>
-          <br/>
-          <h3>{{ LINKS_USER_DATA.FULL_NAME }}</h3>
-          <p>{{ LINKS_USER_DATA.EMAIL }}</p>
-        </v-col>
-        <v-col cols="auto">
-          <h3>{{ LINKS_USER_DATA.ENDERECO }}</h3>
-          <p>{{ LINKS_USER_DATA.ENDERECO_LOCATION }}</p>
-        </v-col>
-      </v-row>
-    </v-container>
-  </v-footer>
+  <div class="footer-main">
+    <v-footer class="mainFooter" padless>
+      <v-container class="d-flex">
+        <v-row no-gutters class="justify-space-around">
+          <v-col class="text-center mt-4 my-auto px-4" cols="7">
+            <h2 class="grey--text">{{ SOBRE_USER_DATA.SOBRE }}</h2>
+            <p class="grey--text text--darken-2">{{ SOBRE_USER_DATA.DESCRIPTION }}</p>
+          </v-col>
+          <v-col cols="auto" class="text-center my-auto px-4">
+            <h3 class="grey--text">{{ LINKS_USER_DATA.LINKS }}</h3>
+            <a :href="LINKS_USER_DATA.LINK_GITHUB" target="_blank">{{ LINKS_USER_DATA.GITHUB }}</a>
+            <v-spacer/>
+            <a :href="LINKS_USER_DATA.LINK_LINKEDIN" target="_blank">
+              {{ LINKS_USER_DATA.LINKEDIN }}
+            </a>
+            <h3 class="grey--text">{{ LINKS_USER_DATA.FULL_NAME }}</h3>
+            <p class="grey--text text--darken-2">{{ LINKS_USER_DATA.EMAIL }}</p>
+          </v-col>
+          <v-col cols="auto" class="text-center my-auto px-4">
+            <h3 class="grey--text">{{ LINKS_USER_DATA.ENDERECO }}</h3>
+            <p class="grey--text text--darken-2">{{ LINKS_USER_DATA.ENDERECO_LOCATION }}</p>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-footer>
+  </div>
 </template>
 
 <script>
@@ -38,25 +38,16 @@ export default {
 };
 </script>
 
-<style>
-.mainFooter {
-  font-family: "Roboto", sans-serif;
-  padding: 50px;
-}
-.mainFooter a:hover {
-  color: #627482;
-}
-.mainFooter h2 {
-  color: #989c9e;
-  font-weight: normal;
-  font-size: 2.5em;
-}
-.mainFooter h3 {
-  font-size: 1em;
-  color: #989c9e;
-}
-.mainFooter p {
-  color: #627482;
-}
+<style lang="scss" scoped>
+.footer-main {
+  .mainFooter {
+    font-family: "Roboto", sans-serif;
+    padding: 50px;
+  }
 
+  .mainFooter h2 {
+    font-weight: normal;
+    font-size: 2.5em;
+  }
+}
 </style>
