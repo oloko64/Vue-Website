ba165a85dc7aa539569bd34db40ef72b5ff1917b
vertocode <evertonvanoni1@gmail.com>
2021-11-01 21:04:39 -0300

    feat: att footer

--- src/components/footer/FooterMain.vue
+++ src/components/footer/FooterMain.vue
@@ -1,27 +1,29 @@
 <template>
-  <v-footer class="black mainFooter" padless>
-    <v-row>
-      <v-col class="text-center mt-4" cols="8">
-        <h2>{{ SOBRE_USER_DATA.SOBRE }}</h2>
-        <p>{{ SOBRE_USER_DATA.DESCRIPTION }}</p>
-      </v-col>
-      <v-col cols="3" class="text-lg-center">
-        <h3>{{ LINKS_USER_DATA.LINKS }}</h3>
-        <a :href="LINKS_USER_DATA.LINK_GITHUB" target="_blank">{{ LINKS_USER_DATA.GITHUB }}</a>
-        <br/>
-        <a :href="LINKS_USER_DATA.LINK_LINKEDIN" target="_blank">
-          {{ LINKS_USER_DATA.LINKEDIN }}
-        </a>
-        <br/>
-        <br/>
-        <h3>{{ LINKS_USER_DATA.FULL_NAME }}</h3>
-        <p>{{ LINKS_USER_DATA.EMAIL }}</p>
-      </v-col>
-      <v-col cols="1">
-        <h3>{{ LINKS_USER_DATA.ENDERECO }}</h3>
-        <p>{{ LINKS_USER_DATA.ENDERECO_LOCATION }}</p>
-      </v-col>
-    </v-row>
+  <v-footer class="mainFooter" padless>
+    <v-container>
+      <v-row no-gutters>
+        <v-col class="text-center mt-4" cols="7">
+          <h2>{{ SOBRE_USER_DATA.SOBRE }}</h2>
+          <p>{{ SOBRE_USER_DATA.DESCRIPTION }}</p>
+        </v-col>
+        <v-col cols="auto" class="text-lg-center">
+          <h3>{{ LINKS_USER_DATA.LINKS }}</h3>
+          <a :href="LINKS_USER_DATA.LINK_GITHUB" target="_blank">{{ LINKS_USER_DATA.GITHUB }}</a>
+          <br/>
+          <a :href="LINKS_USER_DATA.LINK_LINKEDIN" target="_blank">
+            {{ LINKS_USER_DATA.LINKEDIN }}
+          </a>
+          <br/>
+          <br/>
+          <h3>{{ LINKS_USER_DATA.FULL_NAME }}</h3>
+          <p>{{ LINKS_USER_DATA.EMAIL }}</p>
+        </v-col>
+        <v-col cols="auto">
+          <h3>{{ LINKS_USER_DATA.ENDERECO }}</h3>
+          <p>{{ LINKS_USER_DATA.ENDERECO_LOCATION }}</p>
+        </v-col>
+      </v-row>
+    </v-container>
   </v-footer>
 </template>
 
@@ -45,16 +47,16 @@ export default {
   color: #627482;
 }
 .mainFooter h2 {
-  color: #989c9e!important;
+  color: #989c9e;
   font-weight: normal;
   font-size: 2.5em;
 }
 .mainFooter h3 {
   font-size: 1em;
-  color: #989c9e!important;
+  color: #989c9e;
 }
 .mainFooter p {
-  color: #627482!important;
+  color: #627482;
 }
 
 </style>
