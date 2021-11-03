023926a2205a98129145bea51a69bc78e697612c
oloko64 <naldoepingo1@gmail.com>
2021-11-03 20:41:41 -0300

    feat: add page, components, data and routes for recommended books

---
+++ src/data/books/recommendedBooks.js
@@ -0,0 +1,34 @@
+export const BOOK = [
+  {
+    NAME: 'Pro Git',
+    ICON: 'mdi-numeric-1-box',
+    IMAGE_LOCATION: 'https://olokos-place.netlify.app/static/media/livroGit.647a8a4d.jpg',
+    RESUME: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    RESUME_TITLE: 'Lorem ipsum dolor',
+  },
+  {
+    NAME: 'Linux Basics For Hackers',
+    ICON: 'mdi-numeric-2-box',
+    IMAGE_LOCATION: 'https://olokos-place.netlify.app/static/media/livroLinux.aa34afde.jpg',
+    RESUME: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    RESUME_TITLE: 'Lorem ipsum dolor',
+  },
+  {
+    NAME: 'Sistemas Operacionais Modernos',
+    ICON: 'mdi-numeric-3-box',
+    IMAGE_LOCATION: 'https://olokos-place.netlify.app/static/media/livroSO.36d5afda.jpg',
+    RESUME: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    RESUME_TITLE: 'Lorem ipsum dolor',
+  },
+  {
+    NAME: 'Book 4',
+    ICON: 'mdi-numeric-4-box',
+    IMAGE_LOCATION: 'https://olokos-place.netlify.app/static/media/livroLinux.aa34afde.jpg',
+    RESUME: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    RESUME_TITLE: 'Lorem ipsum dolor',
+  },
+];
+
+export const PAGE_TITLE = {
+  TEXT: 'Recommended Books',
+};
