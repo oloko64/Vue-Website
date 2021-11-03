2e83e2a0ad674f4fa965134c504ceda892cd222e
vertocode <evertonvanoni1@gmail.com>
2021-11-02 22:20:06 -0300

    feat: add card component

---
+++ src/components/card/CardCustom.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-card elevation="0" class="cardMain text-center mt-2" outlined>
+    <v-card-title>{{ CARD_DATA[this.cardNumber].TITLE }}</v-card-title>
+    <v-card-text>{{ CARD_DATA[this.cardNumber].DESCRIPTION }}</v-card-text>
+    <a
+      v-if="CARD_DATA[this.cardNumber].HREF"
+      :href="CARD_DATA[this.cardNumber].HREF"
+      target="_blank"
+    >{{ CARD_DATA[this.cardNumber].TYPE_LINK }}
+    </a>
+  </v-card>
+</template>
+
+<script>
+import { CARD_DATA } from '@/data/card/cards';
+
+export default {
+  /*
+  * Define what information will be used inside the card, according to Array publication.
+  */
+  props: {
+    cardNumber: Number,
+  },
+  created() {
+    this.CARD_DATA = CARD_DATA;
+  },
+};
+</script>
+
+<style lang="scss">
+.cardMain {
+  opacity: 0.5;
+}
+</style>
---
+++ src/data/card/cards.js
@@ -0,0 +1,15 @@
+// eslint-disable-next-line import/prefer-default-export
+export const CARD_DATA = [
+  {
+    TITLE: 'FIRST CARD',
+    DESCRIPTION: 'FIRST DESCRIPTION',
+    TYPE_LINK: 'GitHub',
+    HREF: 'github.com',
+  },
+  {
+    TITLE: 'SECOND CARD',
+    DESCRIPTION: 'SECOND DESCRIPTION',
+    TYPE_LINK: 'Linkedin',
+    HREF: 'linkedin.com',
+  },
+];
