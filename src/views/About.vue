10e5d6fa44993068752d433a5ed631ef893232bd
vertocode <evertonvanoni1@gmail.com>
2021-11-02 22:20:06 -0300

    feat: add card component

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,24 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <v-row>
+      <v-spacer />
+      <v-col cols="4">
+        <card-custom card-number="0"/>
+      </v-col>
+      <v-col cols="4">
+        <card-custom card-number="1"/>
+      </v-col>
+      <v-spacer />
+    </v-row>
   </div>
 </template>
+
+<script>
+import CardCustom from '@/components/card/CardCustom.vue';
+
+export default {
+  components: {
+    CardCustom,
+  },
+};
+</script>
