026b6f3526b85d11fddecd7bd6ec40d6b50a45e7
oloko64 <naldoepingo1@gmail.com>
2021-11-04 10:47:31 -0300

    feat: change data structure

--- src/data/menu/navbar.js
+++ src/data/menu/navbar.js
@@ -4,10 +4,30 @@ export const TOP_MENU_DATA = {
   PORTFOLIO: 'BOOKS',
 };
 
-export const DRAWER_DATA = {
-  FIRST: 'GitHub',
-  SECOND: 'Source Code',
-  THIRD: 'Portfólio',
-  FOURTH: 'Buzz',
-  FIFTH: 'Buzz2',
-};
+export const DRAWER_DATA = [
+  {
+    NAME: 'GitHub',
+    ICON: 'mdi-github',
+    LOCATION: 'www.github.com',
+  },
+  {
+    NAME: 'Source Code',
+    ICON: 'mdi-code-not-equal-variant',
+    LOCATION: 'www.github.com',
+  },
+  {
+    NAME: 'Recommended Books',
+    ICON: 'mdi-book',
+    LOCATION: '/#/recommended-books',
+  },
+  {
+    NAME: 'Foo',
+    ICON: 'mdi-information',
+    LOCATION: '',
+  },
+  {
+    NAME: 'Barr',
+    ICON: 'mdi-check',
+    LOCATION: '',
+  },
+];
