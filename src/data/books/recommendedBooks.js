343bd3f9c04b3efdf1f43c35ad738b96c3307fe7
oloko64 <naldoepingo1@gmail.com>
2021-11-04 10:47:31 -0300

    feat: change data structure

--- src/data/books/recommendedBooks.js
+++ src/data/books/recommendedBooks.js
@@ -3,28 +3,92 @@ export const BOOK = [
     NAME: 'Pro Git',
     ICON: 'mdi-numeric-1-box',
     IMAGE_LOCATION: 'https://olokos-place.netlify.app/static/media/livroGit.647a8a4d.jpg',
-    RESUME: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    RESUME: [
+      {
+        PARAGRAPH: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      },
+      {
+        PARAGRAPH: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      },
+      {
+        PARAGRAPH: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      },
+      {
+        PARAGRAPH: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      },
+    ],
     RESUME_TITLE: 'Lorem ipsum dolor',
   },
   {
     NAME: 'Linux Basics For Hackers',
     ICON: 'mdi-numeric-2-box',
     IMAGE_LOCATION: 'https://olokos-place.netlify.app/static/media/livroLinux.aa34afde.jpg',
-    RESUME: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    RESUME: [
+      {
+        ID: 0,
+        PARAGRAPH: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      },
+      {
+        ID: 1,
+        PARAGRAPH: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      },
+      {
+        ID: 2,
+        PARAGRAPH: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      },
+      {
+        ID: 3,
+        PARAGRAPH: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      },
+    ],
     RESUME_TITLE: 'Lorem ipsum dolor',
   },
   {
     NAME: 'Sistemas Operacionais Modernos',
     ICON: 'mdi-numeric-3-box',
     IMAGE_LOCATION: 'https://olokos-place.netlify.app/static/media/livroSO.36d5afda.jpg',
-    RESUME: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    RESUME: [
+      {
+        ID: 0,
+        PARAGRAPH: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      },
+      {
+        ID: 1,
+        PARAGRAPH: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      },
+      {
+        ID: 2,
+        PARAGRAPH: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      },
+      {
+        ID: 3,
+        PARAGRAPH: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      },
+    ],
     RESUME_TITLE: 'Lorem ipsum dolor',
   },
   {
     NAME: 'Book 4',
     ICON: 'mdi-numeric-4-box',
     IMAGE_LOCATION: 'https://olokos-place.netlify.app/static/media/livroLinux.aa34afde.jpg',
-    RESUME: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    RESUME: [
+      {
+        ID: 0,
+        PARAGRAPH: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      },
+      {
+        ID: 1,
+        PARAGRAPH: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      },
+      {
+        ID: 2,
+        PARAGRAPH: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      },
+      {
+        ID: 3,
+        PARAGRAPH: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      },
+    ],
     RESUME_TITLE: 'Lorem ipsum dolor',
   },
 ];
