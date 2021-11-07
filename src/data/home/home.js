47c63de2191fd4966bfd48039b57e2c755442fdc
vertocode <evertonvanoni1@gmail.com>
2021-11-07 15:20:13 -0300

    feat: added services card

--- src/data/home/home.js
+++ src/data/home/home.js
@@ -1,19 +1,64 @@
 // eslint-disable-next-line import/no-unresolved
 import ImageProfile from '../../assets/img/albert-einstein.jpg';
+import vuejs from '../../assets/img/services/vuejs.png';
+import python from '../../assets/img/services/python.png';
+import php from '../../assets/img/services/php.png';
 
 // eslint-disable-next-line import/prefer-default-export
 export const HOME_DATA = {
+  // FIRST CARD DATA
   FULL_NAME: 'Albert Einstein',
   // to choose your profile image, just put the location it is in the import of "ImageProfile"
   IMAGE_PROFILE() {
     return ImageProfile;
   },
-  DESCRIPTION: 'Lorem ipsum dolor sit amet, '
-    + 'consectetur adipiscing elit, '
-    + 'sed do eiusmod tempor incididunt ut '
-    + 'labore et dolore magna aliqua. Ut enim ad '
-    + 'minim veniam, quis nostrud exercitation '
-    + 'ullamco laboris nisi ut aliquip ex ea commodo '
-    + 'consequat. Duis aute irure dolor in reprehenderit '
-    + 'in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+  FIRST_DESCRIPTION: [
+    // eslint-disable-next-line no-multi-str
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit,\
+    sed do eiusmod tempor incididunt ut in voluptate velit esse cillum dolore eu fugiat nulla pariatur\
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit,\
+    sed do eiusmod tempor incididunt ut in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+    // eslint-disable-next-line no-multi-str
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit,\
+    sed do eiusmod tempor incididunt ut in voluptate velit esse cillum dolore eu fugiat nulla pariatur\
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit,\
+    sed do eiusmod tempor incididunt ut in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+    // eslint-disable-next-line no-multi-str
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit,\
+    sed do eiusmod tempor incididunt ut in voluptate velit esse cillum dolore eu fugiat nulla pariatur\
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit,\
+    sed do eiusmod tempor incididunt ut in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+  ],
+  // If you want it to have a second card, answer true, if not, answer false
+  SECOND_CARD: true,
+  // SECOND CARD DATA
+  SECOND_TITLE: 'Services for you',
+  SERVICES_IMG: [
+    vuejs,
+    python,
+    php,
+  ],
+  // To add your image to the array, you need to import at the beginning of this file.
+  SERVICES_TITLE: [
+    'Vue.js',
+    'Python',
+    'PHP',
+  ],
+  SERVICES_DESCRIPTION: [
+    // eslint-disable-next-line no-multi-str
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit,\
+    sed do eiusmod tempor incididunt ut in voluptate velit esse cillum dolore eu fugiat nulla pariatur\
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit,\
+    sed do eiusmod tempor incididunt ut in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+    // eslint-disable-next-line no-multi-str
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit,\
+    sed do eiusmod tempor incididunt ut in voluptate velit esse cillum dolore eu fugiat nulla pariatur\
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit,\
+    sed do eiusmod tempor incididunt ut in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+    // eslint-disable-next-line no-multi-str
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit,\
+    sed do eiusmod tempor incididunt ut in voluptate velit esse cillum dolore eu fugiat nulla pariatur\
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit,\
+    sed do eiusmod tempor incididunt ut in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+  ],
 };
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,34 +1,75 @@
 <template>
-  <div class="Home">
-    <h1 class="text-center mt-4">{{ HOME_DATA.FULL_NAME }}</h1>
-    <v-row  class="my-8">
-      <v-spacer />
-      <v-col align="right" cols="4">
-        <v-img
-          max-height="350"
-          max-width="350"
-          :src="HOME_DATA.IMAGE_PROFILE()"
-        />
-      </v-col>
-      <v-col cols="4">
-        <p>{{ HOME_DATA.DESCRIPTION }}</p>
-      </v-col>
-      <v-spacer />
-    </v-row>
-    <!--
-     it is not necessary to remove
-     this component "in-progress",
-     if you are at work just pass
-     :progress="true",
-     progress default is false.
-   -->
-    <in-progress/>
+  <div class="home">
+    <v-container>
+      <v-card :class="isCardDark" elevation="0" class="text-center card-main__rounded" outlined>
+        <h1 class="text-center mt-4">{{ HOME_DATA.FULL_NAME }}</h1>
+        <v-row no-gutters class="my-8 d-flex justify-center">
+          <v-spacer/>
+          <v-col cols="auto" md="4">
+            <v-img
+              max-height="350"
+              max-width="350"
+              :src="HOME_DATA.IMAGE_PROFILE()"
+            />
+          </v-col>
+          <v-spacer/>
+          <v-col cols="auto" md="6">
+            <p
+              class="text-left mt-5 mx-5 paragraph-indent"
+              v-for="(paragraph, index) in HOME_DATA.FIRST_DESCRIPTION"
+              :key="index"
+            >
+              {{ paragraph }}
+            </p>
+          </v-col>
+          <v-spacer/>
+        </v-row>
+        <!--
+         it is not necessary to remove
+         this component "in-progress",
+         if you are at work just pass
+         :progress="true",
+         progress default is false.
+       -->
+        <in-progress/>
+      </v-card>
+    </v-container>
+    <v-container v-if="HOME_DATA.SECOND_CARD">
+      <v-card :class="isCardDark" elevation="0" class="text-center card-main__rounded" outlined>
+        <h2 class="text-center mt-4">{{ HOME_DATA.SECOND_TITLE }}</h2>
+        <v-row
+          no-gutters
+          class="my-8 d-flex justify-center"
+          v-for="(paragraph, index) in HOME_DATA.SERVICES_DESCRIPTION"
+          :key="index"
+        >
+          <v-col cols="auto" md="2" class="ml-12" align="center">
+            <v-img
+              max-height="100"
+              max-width="100"
+              class="mt-11"
+              :src="HOME_DATA.SERVICES_IMG[index]"
+            />
+          </v-col>
+          <v-col cols="auto" md="9">
+            <div
+              class="text-left mt-9 mx-5 paragraph-indent"
+            >
+              <h3 class="mb-3">{{ HOME_DATA.SERVICES_TITLE[index] }}</h3>
+              <p>
+                {{ paragraph }}
+              </p>
+            </div>
+          </v-col>
+        </v-row>
+      </v-card>
+    </v-container>
   </div>
 </template>
 
 <script>
-import inProgress from '../components/inProgress.vue';
-import { HOME_DATA } from '../data/home/home';
+import inProgress from '@/components/inProgress.vue';
+import { HOME_DATA } from '@/data/home/home';
 
 export default {
   name: 'Home',
@@ -36,8 +77,43 @@ export default {
   components: {
     inProgress,
   },
+  computed: {
+    isCardDark() {
+      return this.$vuetify.theme.dark ? 'card-main__dark' : 'card-main__light';
+    },
+  },
   created() {
     this.HOME_DATA = HOME_DATA;
   },
 };
 </script>
+
+<style lang="scss" scoped>
+.home {
+  .text-link__card {
+    font-size: 16px;
+  }
+
+  .card-main {
+    &__dark:hover {
+      background-color: $color-dark-hover-transparent !important;
+    }
+    &__dark {
+      background-color: $color-dark-transparent !important;
+    }
+    &__light:hover {
+      background-color: $color-light-hover-transparent !important;
+    }
+    &__light {
+      background-color: $color-light-transparent !important;
+    }
+  }
+  .card-main__rounded {
+    border-radius: $border-radius;
+  }
+
+  .paragraph-indent {
+    text-indent: 50px;
+  }
+}
+</style>
