a105297f7c6d7fb0dda3d69616a3bd7c730bad9c
oloko64 <naldoepingo1@gmail.com>
2021-11-07 21:11:46 -0300

    refactor: improve data storage

--- src/data/home/home.js
+++ src/data/home/home.js
@@ -4,37 +4,37 @@ import vuejs from '@/assets/img/services/vuejs.png';
 import python from '@/assets/img/services/python.png';
 import php from '@/assets/img/services/php.png';
 
-// eslint-disable-next-line import/prefer-default-export
 export const HOME_DATA = {
   // FIRST CARD DATA
   FULL_NAME: 'Albert Einstein',
   // to choose your profile image, just put the location it is in the import of "ImageProfile"
-  IMAGE_PROFILE() {
-    return ImageProfile;
-  },
+  IMAGE_PROFILE: ImageProfile,
   FIRST_DESCRIPTION: [
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut in voluptate velit esse cillum dolore eu fugiat nulla pariatur     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut in voluptate velit esse cillum dolore eu fugiat nulla pariatur     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut in voluptate velit esse cillum dolore eu fugiat nulla pariatur     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit,sed do eiusmod tempor incididunt ut in voluptate velit esse cillum dolore eu fugiat nulla pariaturLorem ipsum dolor sit amet, consectetur adipiscing elit,sed do eiusmod tempor incididunt ut in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit,sed do eiusmod tempor incididunt ut in voluptate velit esse cillum dolore eu fugiat nulla pariaturLorem ipsum dolor sit amet, consectetur adipiscing elit,sed do eiusmod tempor incididunt ut in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit,sed do eiusmod tempor incididunt ut in voluptate velit esse cillum dolore eu fugiat nulla pariaturLorem ipsum dolor sit amet, consectetur adipiscing elit,sed do eiusmod tempor incididunt ut in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
   ],
+};
+
+export const SERVICES = {
   // If you want it to have a second card, answer true, if not, answer false
-  SECOND_CARD: true,
-  // SECOND CARD DATA
+  SECOND_CARD_VISIBLE: true,
   SECOND_TITLE: 'Services for you',
-  SERVICES_IMG: [
-    vuejs,
-    python,
-    php,
-  ],
-  // To add your image to the array, you need to import at the beginning of this file.
-  SERVICES_TITLE: [
-    'Vue.js',
-    'Python',
-    'PHP',
-  ],
-  SERVICES_DESCRIPTION: [
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut in voluptate velit esse cillum dolore eu fugiat nulla pariatur     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut in voluptate velit esse cillum dolore eu fugiat nulla pariatur     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut in voluptate velit esse cillum dolore eu fugiat nulla pariatur     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+  SECOND_CARD_DATA: [
+    {
+      IMG: vuejs,
+      TITLE: 'Vue.js',
+      DESCRIPTION: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit,sed do eiusmod tempor incididunt ut in voluptate velit esse cillum dolore eu fugiat nulla pariaturLorem ipsum dolor sit amet, consectetur adipiscing elit,sed do eiusmod tempor incididunt ut in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+    },
+    {
+      IMG: python,
+      TITLE: 'Python',
+      DESCRIPTION: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit,sed do eiusmod tempor incididunt ut in voluptate velit esse cillum dolore eu fugiat nulla pariaturLorem ipsum dolor sit amet, consectetur adipiscing elit,sed do eiusmod tempor incididunt ut in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+    },
+    {
+      IMG: php,
+      TITLE: 'PHP',
+      DESCRIPTION: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit,sed do eiusmod tempor incididunt ut in voluptate velit esse cillum dolore eu fugiat nulla pariaturLorem ipsum dolor sit amet, consectetur adipiscing elit,sed do eiusmod tempor incididunt ut in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+    },
   ],
 };
