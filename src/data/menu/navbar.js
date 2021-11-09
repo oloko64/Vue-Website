e0e8e0aa70ed5f8503a57ec8ef1e86d0c776c9ff
oloko64 <naldoepingo1@gmail.com>
2021-11-08 23:24:07 -0300

    feat: improve data structure

--- src/data/menu/navbar.js
+++ src/data/menu/navbar.js
@@ -1,34 +1,7 @@
+// eslint-disable-next-line import/prefer-default-export
 export const TOP_MENU_DATA = {
   HOME: 'HOME',
   ABOUT: 'ABOUT',
   PORTFOLIO: 'PORTFOLIO',
   BOOKS: 'BOOKS',
 };
-
-export const DRAWER_DATA = [
-  {
-    NAME: 'GitHub',
-    ICON: 'mdi-github',
-    LOCATION: 'www.github.com',
-  },
-  {
-    NAME: 'Source Code',
-    ICON: 'mdi-code-not-equal-variant',
-    LOCATION: 'www.github.com',
-  },
-  {
-    NAME: 'Recommended Books',
-    ICON: 'mdi-book',
-    LOCATION: '/#/recommended-books',
-  },
-  {
-    NAME: 'Foo',
-    ICON: 'mdi-information',
-    LOCATION: '',
-  },
-  {
-    NAME: 'Barr',
-    ICON: 'mdi-check',
-    LOCATION: '',
-  },
-];
