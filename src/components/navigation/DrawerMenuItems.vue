088c76708e10428721dda47ece3069c3a450813e
oloko64 <naldoepingo1@gmail.com>
2021-11-08 23:24:29 -0300

    feat: add functional drawer to app

--- src/components/navigation/DrawerMenuItems.vue
+++ src/components/navigation/DrawerMenuItems.vue
@@ -3,7 +3,11 @@
     <v-list-item-group
       v-model="group"
     >
-      <v-list-item :key="item.NAME" v-for="item in DRAWER_DATA">
+      <v-list-item
+        :key="item.NAME"
+        v-for="item in DRAWER_DATA"
+        @click="handleInsideLocation(item.LOCATION)"
+      >
         <v-icon>{{ item.ICON }}</v-icon>
         <v-list-item-title class="ml-3"> {{ item.NAME }}</v-list-item-title>
       </v-list-item>
@@ -12,7 +16,7 @@
 </template>
 
 <script>
-import { DRAWER_DATA } from '@/data/menu/navbar';
+import { DRAWER_DATA } from '@/data/menu/drawer';
 
 export default {
   name: 'DrawerMenuItems',
@@ -28,6 +32,15 @@ export default {
   },
   created() {
     this.DRAWER_DATA = DRAWER_DATA;
+  },
+  methods: {
+    handleInsideLocation(location) {
+      if (location.includes('http')) {
+        window.open(location);
+      } else if (this.$route.path !== location) {
+        this.$router.push(location);
+      }
+    },
   },
 };
 </script>
