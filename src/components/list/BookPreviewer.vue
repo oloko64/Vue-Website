fc49ae162ea1648c24ac67452ab6d2c851c322dd
oloko64 <naldoepingo1@gmail.com>
2021-11-10 22:29:45 -0300

    feat: add spacers to card

--- src/components/list/BookPreviewer.vue
+++ src/components/list/BookPreviewer.vue
@@ -1,59 +1,59 @@
 <template>
   <div class="book-previewer">
     <v-container>
-    <v-card elevation="0" class=" pa-2 text-center card-main__rounded" outlined>
-      <v-tabs
-        :vertical="!$vuetify.breakpoint.smAndDown"
-        center-active
-        show-arrows
-      >
-        <!-- Book labels -->
-        <v-tab
-          class="justify-start tab-name__max-size"
-          :key="item.NAME"
-          v-for="item in BOOK"
+      <v-card elevation="0" class=" pa-2 my-4 text-center card-main__rounded" outlined>
+        <v-tabs
+          :vertical="!$vuetify.breakpoint.smAndDown"
+          center-active
+          show-arrows
         >
-          <v-icon left>
-            {{ item.ICON }}
-          </v-icon>
-          {{ maxLengthString(item.NAME) }}
-        </v-tab>
+          <!-- Book labels -->
+          <v-tab
+            class="justify-start tab-name__max-size"
+            :key="item.NAME"
+            v-for="item in BOOK"
+          >
+            <v-icon left>
+              {{ item.ICON }}
+            </v-icon>
+            {{ maxLengthString(item.NAME) }}
+          </v-tab>
 
-        <!-- Book details -->
-        <v-tab-item :key="item.NAME" v-for="item in BOOK">
-          <v-card flat>
-            <v-row class="d-flex" no-gutters>
-              <v-col cols="auto" v-if="!$vuetify.breakpoint.smAndDown">
-                <v-img
-                  class="mt-5"
-                  max-width="100px"
-                  :lazy-src="item.IMAGE_LOCATION"
-                  :src="item.IMAGE_LOCATION"
-                />
-                </v-col>
-                <v-col class="justify-center">
-                  <div class="d-flex justify-center" v-if="$vuetify.breakpoint.smAndDown">
-                    <v-img
-                      class="mt-5"
-                      max-width="100px"
-                      :lazy-src="item.IMAGE_LOCATION"
-                      :src="item.IMAGE_LOCATION"
-                    />
-                  </div>
-                  <h2 :class="isSmallScreenTitle" class="mt-5">{{ item.NAME }}</h2>
-                  <v-card-text
-                    class="py-3 paragraph-custom"
-                    :key="paragraph.ID"
-                    v-for="paragraph in item.RESUME"
-                  >
-                    {{ paragraph.PARAGRAPH }}
-                  </v-card-text>
-                </v-col>
-              </v-row>
-            </v-card>
-          </v-tab-item>
-        </v-tabs>
-      </v-card>
+          <!-- Book details -->
+          <v-tab-item :key="item.NAME" v-for="item in BOOK">
+            <v-card flat>
+              <v-row class="d-flex" no-gutters>
+                <v-col cols="auto" v-if="!$vuetify.breakpoint.smAndDown">
+                  <v-img
+                    class="mt-5"
+                    max-width="100px"
+                    :lazy-src="item.IMAGE_LOCATION"
+                    :src="item.IMAGE_LOCATION"
+                  />
+                  </v-col>
+                  <v-col class="justify-center">
+                    <div class="d-flex justify-center" v-if="$vuetify.breakpoint.smAndDown">
+                      <v-img
+                        class="mt-5"
+                        max-width="100px"
+                        :lazy-src="item.IMAGE_LOCATION"
+                        :src="item.IMAGE_LOCATION"
+                      />
+                    </div>
+                    <h2 :class="isSmallScreenTitle" class="mt-5">{{ item.NAME }}</h2>
+                    <v-card-text
+                      class="py-3 paragraph-custom"
+                      :key="paragraph.ID"
+                      v-for="paragraph in item.RESUME"
+                    >
+                      {{ paragraph.PARAGRAPH }}
+                    </v-card-text>
+                  </v-col>
+                </v-row>
+              </v-card>
+            </v-tab-item>
+          </v-tabs>
+        </v-card>
     </v-container>
   </div>
 </template>
