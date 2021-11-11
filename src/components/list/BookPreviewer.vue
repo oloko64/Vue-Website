32f1abcd7b35fcbf4e6e964e80ce8aa14e62a050
oloko64 <naldoepingo1@gmail.com>
2021-11-10 22:22:25 -0300

    feat: change the design of book recommendation

--- src/components/list/BookPreviewer.vue
+++ src/components/list/BookPreviewer.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="book-previewer">
-    <v-card>
+    <v-container>
+    <v-card elevation="0" class=" pa-2 text-center card-main__rounded" outlined>
       <v-tabs
         :vertical="!$vuetify.breakpoint.smAndDown"
         center-active
@@ -29,30 +30,31 @@
                   :lazy-src="item.IMAGE_LOCATION"
                   :src="item.IMAGE_LOCATION"
                 />
-              </v-col>
-              <v-col class="justify-center">
-                <div class="d-flex justify-center" v-if="$vuetify.breakpoint.smAndDown">
-                  <v-img
-                    class="mt-5"
-                    max-width="100px"
-                    :lazy-src="item.IMAGE_LOCATION"
-                    :src="item.IMAGE_LOCATION"
-                  />
-                </div>
-                <h2 :class="isSmallScreenTitle" class="mt-5">{{ item.NAME }}</h2>
-                <v-card-text
-                  class="py-3 paragraph-custom"
-                  :key="paragraph.ID"
-                  v-for="paragraph in item.RESUME"
-                >
-                  {{ paragraph.PARAGRAPH }}
-                </v-card-text>
-              </v-col>
-            </v-row>
-          </v-card>
-        </v-tab-item>
-      </v-tabs>
-    </v-card>
+                </v-col>
+                <v-col class="justify-center">
+                  <div class="d-flex justify-center" v-if="$vuetify.breakpoint.smAndDown">
+                    <v-img
+                      class="mt-5"
+                      max-width="100px"
+                      :lazy-src="item.IMAGE_LOCATION"
+                      :src="item.IMAGE_LOCATION"
+                    />
+                  </div>
+                  <h2 :class="isSmallScreenTitle" class="mt-5">{{ item.NAME }}</h2>
+                  <v-card-text
+                    class="py-3 paragraph-custom"
+                    :key="paragraph.ID"
+                    v-for="paragraph in item.RESUME"
+                  >
+                    {{ paragraph.PARAGRAPH }}
+                  </v-card-text>
+                </v-col>
+              </v-row>
+            </v-card>
+          </v-tab-item>
+        </v-tabs>
+      </v-card>
+    </v-container>
   </div>
 </template>
 
@@ -86,11 +88,14 @@ export default {
 
   .paragraph-custom {
     text-indent: 50px;
-    max-width: 800px;
   }
 
   .tab-name__max-size {
     max-width: 300px;
+  }
+
+  .card-main__rounded {
+    border-radius: $border-radius;
   }
 }
 </style>
